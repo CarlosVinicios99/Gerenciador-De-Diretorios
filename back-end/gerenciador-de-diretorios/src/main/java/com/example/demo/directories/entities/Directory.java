316de4b3af81fb6184dcb91fa52a493ab3c1d361
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-09-21 19:18:17 -0300

    feature: criação do modelo da classe de diretório

---
+++ back-end/gerenciador-de-diretorios/src/main/java/com/example/demo/directories/entities/Directory.java
@@ -0,0 +1,112 @@
+package com.example.demo.directories.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TABLE_DIRECTORIES")
+public class Directory implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	@GeneratedValue
+	@Id
+	private UUID directoryId;
+	
+	@Column
+	private String name;
+	
+	@Column(name = "created_timestamp")
+	private Long createdTimestamp;
+	
+	@Column(name = "created_timestamp")
+	private Long updatedTimestamp;
+	
+	@Column(name = "super_directory_id")
+	private UUID superDirectoryId;
+	
+	
+	public Directory() {
+		
+	}
+
+
+	public Directory(UUID directoryId, String name, Long createdTimestamp, Long updatedTimestamp, UUID superDirectoryId) {
+		this.directoryId = directoryId;
+		this.name = name;
+		this.createdTimestamp = createdTimestamp;
+		this.updatedTimestamp = updatedTimestamp;
+		this.superDirectoryId = superDirectoryId;
+	}
+
+
+	public UUID getDirectoryId() {
+		return directoryId;
+	}
+
+	public void setDirectoryId(UUID directoryId) {
+		this.directoryId = directoryId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getCreatedTimestamp() {
+		return createdTimestamp;
+	}
+
+	public void setCreatedTimestamp(Long createdTimestamp) {
+		this.createdTimestamp = createdTimestamp;
+	}
+
+	public Long getUpdatedTimestamp() {
+		return updatedTimestamp;
+	}
+
+	public void setUpdatedTimestamp(Long updatedTimestamp) {
+		this.updatedTimestamp = updatedTimestamp;
+	}
+
+	public UUID getSuperDirectoryId() {
+		return superDirectoryId;
+	}
+
+	public void setSuperDirectoryId(UUID superDirectoryId) {
+		this.superDirectoryId = superDirectoryId;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(createdTimestamp, directoryId, name, superDirectoryId, updatedTimestamp);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Directory other = (Directory) obj;
+		return Objects.equals(createdTimestamp, other.createdTimestamp)
+			&& Objects.equals(directoryId, other.directoryId) && Objects.equals(name, other.name)
+			&& Objects.equals(superDirectoryId, other.superDirectoryId)
+			&& Objects.equals(updatedTimestamp, other.updatedTimestamp);
+	}
+}
