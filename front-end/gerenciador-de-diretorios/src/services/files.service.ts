837e5cfe12ff7da9dd0183ef7e615a3df064184c
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-09-23 22:53:39 -0300

    feature: implementação dos serviços de arquivo

---
+++ front-end/gerenciador-de-diretorios/src/services/files.service.ts
@@ -0,0 +1,66 @@
+import File from "../models/File"
+
+export default class FilesService {
+
+    private apiURL: string
+
+    constructor(){
+        this.apiURL = process.env.REACT_APP_API_URL || ""
+    }
+  
+    async createFile(name: string, superDirectoryID: string): Promise<File> {
+        try{
+            const path: string = process.env.REACT_APP_PATH_FILES || ""
+            const urlService: string = `${this.apiURL}/${path}`
+
+            const response: any = await fetch(urlService, {
+                method: 'POST',
+                body: JSON.stringify({
+                    name,
+                    superDirectoryID
+                })
+            })
+
+            const file: File = await response.json()
+            return file
+        }
+        catch(error){
+            console.log(`Erro ao criar arquivo. Error: ${error}`)
+            return {} as File
+        }
+    }
+
+    async findFilesByDirectoryId(directoryId: string): Promise<File[]> {
+        try{
+            const path: string = process.env.REACT_APP_PATH_FILES_BY_DIRECTORY || ""
+            const urlService: string = `${this.apiURL}/${path}/${directoryId}`
+
+            const response: any = await fetch(urlService, {
+                method: 'GET'
+            })
+
+            const files: File[] = await response.json()
+            return files
+        }
+        catch(error){
+            console.log(`Erro ao buscar arquivos por diretório. Error: ${error}`)
+            return []        
+        }
+    }
+
+    async deleteFileById(fileId: string): Promise<void> {
+        try{
+            const path: string = process.env.REACT_APP_PATH_FILES || ""
+            const urlService: string = `${this.apiURL}/${path}/${fileId}`
+            console.log(urlService)
+
+            await fetch(urlService, {
+                method: 'DELETE'
+            })
+        }
+        catch(error){
+            console.log(`Erro ao deletar arquivo com ID ${fileId}. Error: ${error}`)
+        }
+    }
+
+}
